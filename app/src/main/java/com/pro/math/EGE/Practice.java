207fba5e72866cbb0fc8c387c3c4c98d409c3caa
Shwerch <suspizda0@gmail.com>
2024-04-14 14:29:16 +0600

    practice build v0.15

--- app/src/main/java/com/pro/math/EGE/Practice.java
+++ app/src/main/java/com/pro/math/EGE/Practice.java
@@ -1,5 +1,23 @@
 package com.pro.math.EGE;
 
-public class Practice {
+import java.util.ArrayList;
+import java.util.TreeMap;
 
+public class Practice {
+    private static final byte TASKS = 19;
+    public static ArrayList<String>[] TasksAnnotations = new ArrayList[TASKS];
+    public static ArrayList<String>[] TasksTexts = new ArrayList[TASKS];
+    public static ArrayList<String>[] TasksAnswers = new ArrayList[TASKS];
+    public static ArrayList<String>[] TasksSolutions = new ArrayList[TASKS];
+    public static ArrayList<Integer>[] TasksRewards = new ArrayList[TASKS];
+    private static void AddTask(int TaskNumber,String Annotation,String Test,String Answer,String Solution,int Reward) {
+        TasksAnnotations[TaskNumber].add(Annotation);
+        TasksTexts[TaskNumber].add(Test);
+        TasksAnswers[TaskNumber].add(Answer);
+        TasksSolutions[TaskNumber].add(Solution);
+        TasksRewards[TaskNumber].add(Reward);
+    }
+    static {
+
+    }
 }
