340256bf568a6e309c3e5c04cfa6ede4d36e9ac9
Shwerch <suspizda0@gmail.com>
2024-05-13 21:39:54 +0700

    deleted unused classes

--- app/src/main/java/com/pro/math/EGE/Products/Product.java
+++
@@ -1,9 +0,0 @@
-package com.pro.math.EGE.Products;
-
-public class Product extends AbstractProduct {
-    public int topicId, subTopicId;
-    public Product(int topicId, int subTopicId) {
-        this.topicId = topicId;
-        this.subTopicId = subTopicId;
-    }
-}
--- app/src/main/java/com/pro/math/EGE/Products/SubTopic.java
+++ app/src/main/java/com/pro/math/EGE/Products/SubTopic.java
@@ -1,7 +1,9 @@
 package com.pro.math.EGE.Products;
 
-public class SubTopic extends AbstractProduct {
-    public int topicId, subTopicId, cost, tasksCount,reward;
+import androidx.annotation.NonNull;
+
+public class SubTopic implements Comparable<SubTopic> {
+    public int topicId, subTopicId, cost, tasksCount, reward;
     public boolean availability;
     public SubTopic(int topicId, int subTopicId, boolean availability, int cost, int tasksCount, int reward) {
         this.topicId = topicId;
@@ -10,5 +12,17 @@ public class SubTopic extends AbstractProduct {
         this.cost = cost;
         this.tasksCount = tasksCount;
         this.reward = reward;
+    }
+    @Override
+    public int compareTo(@NonNull SubTopic product) {
+        if (this.topicId > product.topicId)
+            return 1;
+        else if (this.topicId < product.topicId)
+            return -1;
+        else if (this.subTopicId > product.subTopicId)
+            return 1;
+        else if (this.subTopicId < product.subTopicId)
+            return -1;
+        return 0;
     }
 }
